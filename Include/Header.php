081eb8596c9ed41911e62006816582e6a6260451
yasser nadir <yassernadir761@gmail.com>
2021-01-06 00:32:52 +0100

    update header -navbar-

--- Include/Header.php
+++ Include/Header.php
@@ -11,18 +11,20 @@
     <link rel="stylesheet" href="Static/bootstrap.min.css">
     <title><?=$title?></title>
     <style>
-        
+        .logo{
+            border-left: 5px solid white;
+            border-radius: 2px;
+            padding-left: 10px;
+            font-size: 2em;
+        }
     </style>
 </head>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary py-1">
-    <a href="index.php" class="navbar-brand mt-1"><h2>ENSET</h2></a>
+    <a href="index.php" class="navbar-brand mt-1"><h2 class="logo">ENSET</h2></a>
     <div class="collapse navbar-collapse justify-content-end">
         <ul class="navbar-nav">
         <?php if(isset($_SESSION["auth"])): ?>
-            <li class="nav-item">
-                <a href="http://localhost/stage/editProfil.php" class="nav-link active">Mon profile</a><!--editProfil.php-->
-            </li>
             <li class="nav-item">
                 <a href="desconnect.php" class="nav-link active">Se deconnecter</a>
             </li>
